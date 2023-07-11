85cd0d88e13bd9054eed1279da680fa7b04ac9f3
allansmithll <allanammancio2004@gmail.com>
2023-07-10 22:00:25 -0300

    05  - Criação de índices para a aplicação

---
+++ Exercises/05-indexes.txt
@@ -0,0 +1,2 @@
+create index data_pag_idx on pagamento(data);
+create index num_unid_mora_idx on morador(numerounidade, blocounidade);
--- Scripts/Inserts/insertsMorador.sql
+++ Scripts/Inserts/insertsMorador.sql
@@ -1,8 +1,8 @@
 -- Morador comum
-call inserirMorador('12345678900', 'Maria Santos', 'maria.santos@example.com', 1, ['+55083982291213']);
-call inserirMorador('45678976556', 'Allan Amancio', 'allanammancio04@example.com', 2, ['+55083982291235']);
+call inserirMorador('12345678900', 'Maria Santos', 'maria.santos@example.com', 1, 'A', array['+55083982291213']);
+call inserirMorador('45678976556', 'Allan Amancio', 'allanammancio04@example.com', 2, 'A', array['+55083982291235']);
+call inserirMorador('98765432100', 'João Silva', 'joao.silva@example.com', 1, 'A', array['+55081954678907']);
 select * from morador;
-call inserirMorador('98765432100', 'João Silva', 'joao.silva@example.com', 1);
 
 call inserirSindico('45678976556',true,'2023-01-01','2025-01-01','20232025','2023-01-01','2025-01-01','Primeiro mandato de Allan Amancio');
 select * from sindico;
